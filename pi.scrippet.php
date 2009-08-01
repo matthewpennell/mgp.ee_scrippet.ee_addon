a8ca549c46a3387fb494ac4087079d7674e2444f
matthewpennell <github@matthewpennell.com>
2009-08-01 00:13:26 -0700

    Fix character name bug as per Wordpress plugin v1.4.

--- pi.scrippet.php
+++ pi.scrippet.php
@@ -14,7 +14,7 @@
 =====================================================
  File: pi.scrippet.php
 -----------------------------------------------------
- Purpose: Scropt formatting text block
+ Purpose: Script formatting text block
 =====================================================
 
 */
@@ -95,7 +95,7 @@ class Scrippet {
 		$replace   = array('', '&', '&#46;&#46;&#46;', '&#45;&#45;');
 		
 		// Sceneheaders must start with INT, EXT, or EST
-		$pattern[] = '/(INT|EXT|[^a-zA-Z0-9]EST)([\.\-\s]+?)(.+?)([A-Za-z0-9\)\s\.])\n/';
+		$pattern[] = '/\n(INT|EXT|[^a-zA-Z0-9]EST)([\.\-\s]+?)(.+?)([A-Za-z0-9\)\s\.])\n/';
 		$replace[] = '<p class="sceneheader">\1\2\3\4</p>' . "\n";
 		
 		// Catches transitions
